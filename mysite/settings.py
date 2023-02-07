77d3fbb3eb9d3e921823fc2ce14a5cf6c942a311
matuta <fll.sendai.02@gmail.com>
2023-02-07 20:45:21 +0900

    htmlの写真追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / "images"
